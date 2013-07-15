e75ceb158a2081ced45b7b060fff78ae8eaf3db8
moly <tsweston@gmail.com>
2013-07-15 10:40:39 +0100

    started gwt example

---
+++ flixel-examples-html5/src/org/flixel/client/FlxHtml5ExampleStarter.java
@@ -0,0 +1,11 @@
+package org.flixel.client;
+
+import org.flixel.examples.animation.AnimationDemo;
+
+public class FlxHtml5ExampleStarter extends FlxHtml5Application
+{
+	public FlxHtml5ExampleStarter()
+	{
+		super(new AnimationDemo(), 400, 240);
+	}
+}
