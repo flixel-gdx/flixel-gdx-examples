a60fd0663ad13e96323dad7aaaa778282bdf6c22
WingEraser <kawingchin@gmail.com>
2014-01-10 18:48:49 +0100

    Updated UI tests.

--- flixel-examples-core/src/org/flixel/examples/userinterface/LabelState.java
+++ flixel-examples-core/src/org/flixel/examples/userinterface/LabelState.java
@@ -1,6 +1,8 @@
 package org.flixel.examples.userinterface;
 
 import org.flixel.ui.FlxLabel;
+import org.flixel.ui.FlxNinePatch;
+import org.flixel.ui.FlxUISkin;
 
 /**
  *
@@ -8,6 +10,8 @@ import org.flixel.ui.FlxLabel;
  */
 public class LabelState extends Test
 {
+	private static final String ImgLabelCenter = "examples/userinterface/custom_skin/ninepatch_label_middlecenter.png";
+	
 	 @Override
 	public void create()
 	{
@@ -16,7 +20,22 @@ public class LabelState extends Test
 		add(new FlxLabel(10, 10, null, "flixel label", 0, 0));
 		add(new FlxLabel(10, 50, null, "label", 200, 0));
 		add(new FlxLabel(10, 100, null, "label", 50, 20));
-		add(new FlxLabel(10, 150, null, "label", 100, 30));
+		add(new FlxLabel(10, 150, null, "label", 100, 50));
+		
+		
+		FlxUISkin skin = new FlxUISkin();
+		skin = new FlxUISkin();
+		skin.HIGHLIGHT = -1;
+		skin.PRESSED = -1;
+		skin.DISABLED = -1;		
+		skin.setFormat(null, 16, 0x333333, "center");
+		skin.labelVerticalAlign = "middle";
+		skin.setNinePatch(FlxNinePatch.MIDDLE_CENTER, ImgLabelCenter, 50, 50, true);
+		skin.labelOffset.y = 0;
+		skin.labelOffset.x = -2;
+		
+		FlxLabel label = new FlxLabel(5, 220, skin, "Repeated Background", 250, 50);
+		add(label);
 	}
 }
 
