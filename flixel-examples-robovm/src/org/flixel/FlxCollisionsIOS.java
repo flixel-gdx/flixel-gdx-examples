fd0fd8fdd1d921bb926dd2e776fa39c9aa692f07
Robert Ragas <sukk1e@Robert-Ragass-Mac-mini.local>
2014-02-09 22:01:29 +0100

    Runs Mode and FlxCollisions. Edit robovm.properties to run different demo.

---
+++ flixel-examples-robovm/src/org/flixel/FlxCollisionsIOS.java
@@ -0,0 +1,29 @@
+package org.flixel;
+
+import org.flixel.examples.flxcollisions.FlxCollisions;
+import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
+import org.robovm.cocoatouch.uikit.UIApplication;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+public class FlxCollisionsIOS extends IOSApplication.Delegate
+{
+	public static void main(String[] args)
+	{
+		NSAutoreleasePool pool = new NSAutoreleasePool();
+		UIApplication.main(args, null, FlxCollisionsIOS.class);
+		pool.drain();		
+	}
+
+	@Override
+	protected IOSApplication createApplication()
+	{
+		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+		config.orientationLandscape = true;
+		config.orientationPortrait = false;
+		FlxGame game = new FlxCollisions();
+		return new IOSApplication((ApplicationListener) game.stage, config);
+	}
+}
---
+++ flixel-examples-robovm/src/org/flixel/ModeIOS.java
@@ -0,0 +1,29 @@
+package org.flixel;
+
+import org.flixel.examples.mode.Mode;
+import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
+import org.robovm.cocoatouch.uikit.UIApplication;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+public class ModeIOS extends IOSApplication.Delegate
+{
+	@Override
+	protected IOSApplication createApplication()
+	{
+		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+		config.orientationLandscape = true;
+		config.orientationPortrait = false;
+		FlxGame game = new Mode();
+		return new IOSApplication((ApplicationListener) game.stage, config);
+	}
+
+	public static void main(String[] argv)
+	{
+		NSAutoreleasePool pool = new NSAutoreleasePool();
+		UIApplication.main(argv, null, ModeIOS.class);
+		pool.drain();
+	}
+}
