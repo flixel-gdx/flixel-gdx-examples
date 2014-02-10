cf198d460f0544af778d129415861789fced569d
WingEraser <kawingchin@gmail.com>
2014-02-10 18:58:40 +0100

    Added TiledMap2Demo to -robovm (to test tmx)

---
+++ flixel-examples-robovm/src/org/flixel/TiledMap2DemoIOS.java
@@ -0,0 +1,29 @@
+package org.flixel;
+
+import org.flixel.examples.tiledmap2.TiledMap2Demo;
+import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
+import org.robovm.cocoatouch.uikit.UIApplication;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+
+public class TiledMap2DemoIOS extends IOSApplication.Delegate
+{
+	@Override
+	protected IOSApplication createApplication()
+	{
+		IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+		config.orientationLandscape = true;
+		config.orientationPortrait = false;
+		FlxGame game = new TiledMap2Demo();
+		return new IOSApplication((ApplicationListener) game.stage, config);
+	}
+
+	public static void main(String[] argv)
+	{
+		NSAutoreleasePool pool = new NSAutoreleasePool();
+		UIApplication.main(argv, null, TiledMap2DemoIOS.class);
+		pool.drain();
+	}
+}
